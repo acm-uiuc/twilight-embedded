c7ac9910c344ed907e73f74f29a636d5b705561f
Naren Dasan <naren@narendasan.com>
2017-12-01 14:53:22 -0600

    more changes

--- PIC/tests/i2c_test/i2c_test_slave/i2c_test_slave.ino
+++ PIC/tests/i2c_test/i2c_test_slave/i2c_test_slave.ino
@@ -1,24 +1,27 @@
 #include <Wire.h>
 
+#define PIN 9
 String data = "";
 
 void setup() {
     Wire.begin(0x12);
     Wire.onReceive(receiveData);
     Wire.onRequest(sendData);
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {}
 
 void receiveData(int bytes) {
-      data = "";
-  while( Wire.available()){
-    data += (char)Wire.read();
-  }
+    data = "";
+    while( Wire.available()){
+        data += (char)Wire.read();
+    }
+    digitalWrite(PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
 }
-
 void sendData() {
     Wire.write(data.c_str());
 }
+
 
 
