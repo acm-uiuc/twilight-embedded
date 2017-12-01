674315832d1ff1e0b01ab66cd0571740114386a3
Naren Dasan <naren@narendasan.com>
2017-12-01 11:21:21 -0600

    new test

--- PIC/tests/i2c_test/i2c_test_slave/i2c_test_slave.ino
+++ PIC/tests/i2c_test/i2c_test_slave/i2c_test_slave.ino
@@ -1,22 +1,9 @@
 #include <Wire.h>
 
-static const char messageInterrupt = A0;
-static const char numberInterrupt = A1;
-
-int flag = 0;
-
 void setup() {
-    pinMode(numberInterrupt, OUTPUT);
-    pinMode(messageInterrupt, OUTPUT);
-
-    Serial.begin(9600);
-
     Wire.begin(0x12);
     Wire.onReceive(receiveData);
     Wire.onRequest(sendData);
-    digitalWrite(numberInterrupt, LOW);
-    digitalWrite(messageInterrupt, LOW);
-
 }
 
 void loop() {}
@@ -24,15 +11,6 @@ void loop() {}
 void receiveData(int bytes) {}
 
 void sendData() {
-    if(flag == 2) {
-      Wire.write("Hello foo");
-      delay(1000);
-    }
-
-    if(flag == 1) {
-        Wire.write("Hello bar");
-        delay(1000);  //if no sync : add more delay.
-    }
-
-    flag = 0;
+    Wire.write("Hello foo");
 }
+
