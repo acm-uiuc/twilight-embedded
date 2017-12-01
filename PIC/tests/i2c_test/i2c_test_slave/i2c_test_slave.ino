cb2864f7078cdcf38d03ba9c71ec34a7c6e33695
Naren Dasan <naren@narendasan.com>
2017-12-01 11:52:02 -0600

    more changes

--- PIC/tests/i2c_test/i2c_test_slave/i2c_test_slave.ino
+++ PIC/tests/i2c_test/i2c_test_slave/i2c_test_slave.ino
@@ -1,5 +1,7 @@
 #include <Wire.h>
 
+String data = "";
+
 void setup() {
     Wire.begin(0x12);
     Wire.onReceive(receiveData);
@@ -8,9 +10,15 @@ void setup() {
 
 void loop() {}
 
-void receiveData(int bytes) {}
+void receiveData(int bytes) {
+      data = "";
+  while( Wire.available()){
+    data += (char)Wire.read();
+  }
+}
 
 void sendData() {
-    Wire.write("Hello foo");
+    Wire.write(data.c_str());
 }
+
 
