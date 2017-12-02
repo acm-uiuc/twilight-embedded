3b80d6d0fa9d582358631f8bff8ae731fd4679c2
Naren Dasan <naren@narendasan.com>
2017-12-01 18:48:15 -0600

    better setup script

--- utils/setup/setup_device.sh
+++ utils/setup/setup_device.sh
@@ -8,11 +8,11 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 sudo rm /etc/hosts
 sudo cp $DIR/hosts.template  /etc/hosts
-sudo echo -e "127.0.1.1	        twilight_$1" >> /etc/hosts
+sudo echo -e "127.0.1.1	      twilight_$1" >> /etc/hosts
 sudo echo -e "twilight_$1" > /etc/hostname
 
 sudo apt update
 sudo apt install -y avrdude python-smbus i2c-tools
 avrdude -v
-cat /etc/avrdude.conf avr_programming_pins.conf > ~/avrdude_gpio.conf
+cat /etc/avrdude.conf $DIR/avr_programming_pins.conf > ~/avrdude_gpio.conf
 echo -e "Setup done, make sure to enable I2C with raspi-config and set the fuses with set_fuses and reboot"
