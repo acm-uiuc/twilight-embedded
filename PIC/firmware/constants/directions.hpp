13850d9786897c2b4fe6399a1d71b9fd55577c32
Naren Dasan <naren@narendasan.com>
2017-12-02 17:24:48 -0600

    new build

--- PIC/firmware/constants/directions.hpp
+++ PIC/firmware/constants/directions.hpp
@@ -1,10 +1,10 @@
 #ifndef DIRECTIONS_H
 #define DIRECTIONS_H
 
-#define SELF  0
-#define NORTH 1
-#define SOUTH 2
-//#define EAST 3
-//#define WEST 4
+#define SELF  5
+#define NORTH 2
+#define SOUTH 3
+//#define EAST 1
+//#define WEST 0
 
 #endif //DIRECTIONS_H
