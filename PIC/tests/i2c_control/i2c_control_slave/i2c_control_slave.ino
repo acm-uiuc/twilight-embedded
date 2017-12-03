08a4c6333abad2ec991c30c6f022021c65b8a99a
Naren Dasan <naren@narendasan.com>
2017-12-03 00:50:05 -0600

    commit message

--- PIC/tests/i2c_control/i2c_control_slave/i2c_control_slave.ino
+++ PIC/tests/i2c_control/i2c_control_slave/i2c_control_slave.ino
@@ -21,8 +21,8 @@ void loop() {
     if (byteArrayChanged) {
         frame.SetColor(100, 12, 80);
         frame.UpdateFrame();
-        frame.ApplyCommand(byteArray);
-        frame.UpdateFrame();
+        //frame.ApplyCommand(byteArray);
+        //frame.UpdateFrame();
         byteArrayChanged = 0;
     }
 }
