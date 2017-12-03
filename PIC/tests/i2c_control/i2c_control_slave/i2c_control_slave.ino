3d52501796db1668f6f94919e9c3400f63eb7559
Naren Dasan <naren@narendasan.com>
2017-12-03 14:32:01 -0600

    commit message

--- PIC/tests/i2c_control/i2c_control_slave/i2c_control_slave.ino
+++ PIC/tests/i2c_control/i2c_control_slave/i2c_control_slave.ino
@@ -2,10 +2,9 @@
 #include <Wire.h>
 
 #define SIZE 64
-#define EOT  4
+
 int count = 0;
 char byteArray[SIZE];
-char outputStr[SIZE];
 int byteArrayChanged = 0;
 
 void setup() {
@@ -31,16 +30,15 @@ void receiveEvent(int howMany) {
 
     while (Wire.available()) {
         int readByte = Wire.read();
-        if (readByte == EOT) {
-            byteArray[count] = 0;
+        byteArray[count] = readByte;
+
+        if (readByte == 0) {
             count = 0;
             byteArrayChanged = 1;
-            break;
+        } else {
+            count++;
+            byteArrayChanged = 0;
         }
-
-        byteArrayChanged = 0;
-        byteArray[count] = readByte;
-        count++;
 
         if (count == SIZE) {
             count = 0;
