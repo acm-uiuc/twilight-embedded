4ae14317ff291a3ff86cc3d3f8db2cac0551d4c4
Naren Dasan <naren@narendasan.com>
2017-12-03 00:21:11 -0600

    commit message

--- PIC/tests/i2c_control/i2c_control_slave/i2c_control_slave.ino
+++ PIC/tests/i2c_control/i2c_control_slave/i2c_control_slave.ino
@@ -6,6 +6,7 @@ char byteArray[16];
 int byteArrayChanged = 0;
 
 void setup() {
+
     Wire.begin(0x12);
     Wire.onReceive(receiveEvent);
     Wire.onRequest(sendData);
@@ -17,26 +18,26 @@ void setup() {
 
 void loop() {
     if (byteArrayChanged) {
-        apply_frame_command(String(byteArray));
+        frame.ApplyCommand(byteArray);
+        frame.UpdateFrame();
         byteArrayChanged = 0;
     }
 }
 
 void receiveEvent(int howMany) {
-      frame.SetColor(255, 0, 0);
-    frame.UpdateFrame();
- while(Wire.available()) {
-      if(count < 16){
-        byteArray[count] = Wire.read();
-        count++;
-      }
-      else{
-        count = 0;
-        byteArray[count] = Wire.read();
-      }
-    }
-    count = 0;        // print the integer
+    count = 0;
+    memset(byteArray, 0, 16);
     byteArrayChanged = 1;
+    while(Wire.available()) {
+        if (count < 16) {
+            byteArray[count] = Wire.read();
+            count++;
+        } else {
+            break;
+            count = 0;
+            byteArray[count] = Wire.read();
+        }
+    }
 }
 
 void sendData() {
