2454a4ba648b298bf0290ceb99a021ea7a9fe748
Naren Dasan <naren@narendasan.com>
2017-12-03 19:14:07 -0600

    commit message

--- PIC/tests/test_cmd.py
+++ PIC/tests/test_cmd.py
@@ -1,3 +1,50 @@
+
+import smbus
+import argparse
+
+PARSER = argparse.ArgumentParser(description=None)
+
+PARSER.add_argument("cmd", type=str)
+
+
+ARGS = PARSER.parse_args()
+
+
+
+i2c = smbus.SMBus(1)
+slave = 0x12
+
+CMD = ARGS.cmd
+
+def send_str(str): 
+    i2c.write_byte(slave, 2)
+    for b in str:
+        i2c.write_byte(slave, ord(b))
+    i2c.write_byte(slave, 3)
+
+def read(): 
+    data_received_from_Arduino = i2c.read_i2c_block_data(slave, 0,16)
+    print '[{}]'.format(', '.join(hex(x) for x in data_received_from_Arduino))
+    str_in = "" 
+    for c in data_received_from_Arduino:
+        if c != 0xFF:
+            str_in += chr(c)
+    return str_in
+
+if CMD == "read":
+    LED_CMD = read()
+    if LED_CMD.startswith("COM:"):
+        CMD = LED_CMD.split("COM:")[1]
+        send_str(CMD)
+else:
+    send_str(CMD)
+
+
+
+
+
+
+'''
 import smbus
 from random import randint
 import platform
@@ -38,3 +85,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+'''
